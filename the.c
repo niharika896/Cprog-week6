24dd8053cace1fe25695ccceb10c60d006cf5fec
niharika896 <niharika9agarwal@gmail.com>
2024-11-23 11:54:10 +0530

    Update the.c

--- the.c
+++ the.c
@@ -1,31 +1,18 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-
-int main() {
-    char ch[] = "hello the nice the day the.";
-    int n = strlen(ch), count = 0;
-    char t[] = "the";
-    int m = strlen(t);
-
-    for (int j = 0; j <= n - m; j++) {
-        if (strncmp(&ch[j], t, m) == 0) {
-            if ((j == 0 || isspace(ch[j - 1])) && 
-                (ch[j + m] == '\0' || isspace(ch[j + m]) || ch[j + m] == '.')) {
-                count++;
-            }
+int main(){
+    int count=0;
+    char t[]="the";
+    char sen[100];
+    fgets(sen,sizeof(sen),stdin);
+    for(int i=0;sen[i]!='\0';i++){
+        int found=1;
+        for(int j=0;j<3;j++){
+            if(sen[i+j]!=t[j])found=0;
         }
+        if(found) if(sen[i+3]==' '||sen[i+3]=='.')
+        count++;
     }
-
-    printf("%d\n", count);
+    printf("%d",count);
     return 0;
 }
-
-
-// #include <stdio.h>
-// #include <string.h>
-// int main(){
-//     char ch[]="ab";
-//     printf("%d",ch[0]=='a');
-//     return 0;
-// }
