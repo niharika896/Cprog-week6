e434ff4ce726c6d09035fe173a20482e8c14d2e8
niharika896 <niharika9agarwal@gmail.com>
2024-12-25 19:33:09 +0530

    Update firstnonrep.c

--- firstnonrep.c
+++ firstnonrep.c
@@ -6,20 +6,25 @@ void firstNonrep(char *str) {
     for (i = 0; str[i] != '\0'; i++) {
         found = 0;
         for (j = 0; str[j] != '\0'; j++) {
-            if (str[i] == str[j]) {
+            if (str[i] == str[j] && i != j) { 
+                found = 1;
                 break;
             }
         }
-        printf("First non-repeating character is: %c\n", str[i]);
-        return;
+        if (!found) {
+            printf("First non-repeating character is: %c\n", str[i]);
+            return; 
         }
-    printf("No non-repeating character found.\n");
     }
+    printf("No non-repeating character found.\n");
+}
 
 int main() {
     char str[256];
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
+    
+
     firstNonrep(str);
 
     return 0;
