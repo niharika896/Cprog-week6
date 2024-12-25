950f986d47335e3b48187714e45764f2a6fbe29a
niharika896 <niharika9agarwal@gmail.com>
2024-12-25 20:09:28 +0530

    Update remove_repeat.c

--- remove_repeat.c
+++ remove_repeat.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
 #include <string.h>
-
-void removeRepeatedChars(char *str) {
-	int i,j,c=0,k;
-	for(i=0;str[i]!='\0';i++){
-		for(j=i+1;str[j]!='\0';j++)
-			if(str[i]==str[j])
-                for (k = j; str[k] != '\0'; k++) {
-                str[k] = str[k+ 1];
+int main(){
+	char str[100];
+    fgets(str,sizeof(str),stdin);
+    int n=strlen(str);
+    for(int i=n-1;i>=1;i--){
+        for(int j=i-1;j>=0;j--){
+            if(str[i]==str[j]){
+                int k;
+                for(k=i;str[k]!='\0';k++)str[k]=str[k+1];
+                str[k-1]='\0';
             }
-            j--;
-	};      
+        }
+    }
+    for(int i=0;str[i]!='\0';i++)printf("%c",str[i]);
+	return 0;
 }
-
-int main() {
-    char str[256],i;
-    printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin);
-	removeRepeatedChars(str);
-    for(i=0;i<strlen(str);i++)
-    if(str[i]!=';')printf("%c",str[i]);
-
-    return 0;
-}
-
